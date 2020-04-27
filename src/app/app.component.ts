89afd2b6060e70aeac2f26a2ec118a0961816a79
Ganesh Khaire <gnshkhr@gmail.com>
2020-04-27 12:54:45 +0530

    first commit adding folder of angular by sid

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { UserService } from './services/user/user.service';
+import { VoterComponent } from './components/voter/voter.component';
+import { Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // providers: [ UserService ]
+})
+export class AppComponent {
+
+}
